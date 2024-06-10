edf5a651dcea2e1c1fb3b7ddc0474e42dae3435f
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-06-10 16:19:14 +0000

    tried debugging

--- app/controllers/multiplication_controller.rb
+++ app/controllers/multiplication_controller.rb
@@ -6,8 +6,8 @@ class MultiplicationController < ApplicationController
   def multiply_these
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-
-    @result =  @first_number * @second_number
+    
+    @result = @first_number * @second_number
 
     render({ :template => "multiplication_templates/multiply_results" })
   end
